1114a8a414914d53646091d19eea59dde6d4adee
AdrienRoux <35739752+AdrienRoux@users.noreply.github.com>
2018-04-07 17:53:26 +0200

    Delete args-info.js

--- args-info.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'args-info',
-  description: 'Information about the arguments provided.',
-  args: true,
-  guildOnly: false,
-  execute(message, args) {
-    message.channel.send(`Arguments: ${args}\nArguments length: ${args.length}`);
-  },
-};
